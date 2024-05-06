5e457dffe232703f1375a69f6cc5252615583be2
WilfullMurder <mrfarrow2013@gmail.com>
2024-05-06 15:09:39 +0100

    updates scripting for the new dropdown menu.
    implements new dropdown menu
    in index.html and other
    pages.
    
    Swaps Marinas around

--- static/js/main.js
+++ static/js/main.js
@@ -1,21 +1,8 @@
 $(document).ready(function(){
     $("#footer").load("footer.html");
-});
-
-$(document).ready(function(){
-    $("#menu").load("menu.html");
-});
-
-
-$(document).ready(function(){
-    $("#footer").load("footer.html");
-
-    document.querySelector('.burger-menu').addEventListener('click', function() {
-        var menu = document.querySelector('.menu');
-        if (menu.style.display === 'none' || menu.style.display === '') {
-            menu.style.display = 'flex';
-        } else {
-            menu.style.display = 'none';
-        }
+    $("#menu").load("menu.html", function() {
+        $('.burger-menu').on('click', function() {
+            $('.dropdown-menu').toggle();
+        });
     });
 });
