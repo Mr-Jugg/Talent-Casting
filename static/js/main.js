dc8754dd0316cc1436d18012cbeaae8ebd2b5bb7
WilfullMurder <mrfarrow2013@gmail.com>
2024-05-08 19:38:53 +0100

    tweaks menu styles so dropdown scrolls with page updates script so menu closes if anywhere else is pressed

--- static/js/main.js
+++ static/js/main.js
@@ -3,6 +3,15 @@ $(document).ready(function(){
     $("#menu").load("menu.html", function() {
         $('.burger-menu').on('click', function() {
             $('.dropdown-menu').toggle();
+            });
+
+      // Add event listener to the document
+            $(document).on('click', function(event) {
+                // Check if the target of the click event is outside the burger menu
+                if (!$(event.target).closest('.burger-menu').length) {
+                    // If it is, close the burger menu
+                    $('.dropdown-menu').hide();
+                }
         });
     });
 });
